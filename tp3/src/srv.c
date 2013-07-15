716d72fe221a52feace9c8ff50f413abddd7addd
luki <luki@luki-TF-570-SLI.(none)>
2013-07-14 22:50:27 -0300

    arreglado el bug

--- tp3/src/srv.c
+++ tp3/src/srv.c
@@ -3,11 +3,13 @@
 
 void servidor(int mi_cliente)
 {
-    MPI_Status statusRcv; int origen, tag;
+    MPI_Status status; int origen, tag;
     int hay_pedido_local = FALSE;
+    int tengo_el_lock = FALSE;
     int listo_para_salir = FALSE;
     //char buffer[100];
     int our_sequence_number;
+    int sequence_number;
     int highest_sequence_number_seen = 0;
     int responses;
     int n_ranks, mi_rank;
@@ -19,44 +21,35 @@ void servidor(int mi_cliente)
     mi_rank = mi_rank/2;
     int withClients =  n_ranks;
    	int delayedResponses[n_ranks];
-	MPI_Request requests[n_ranks];
-	MPI_Status status[n_ranks];
-	int sentMessage[n_ranks];
+	MPI_Request request;
 
   	int id;
    	for (id = 0; id < n_ranks; id++){
    		delayedResponses[id] = FALSE;
-   		sentMessage[id] = FALSE;
    	}
     
     while( ! listo_para_salir ) {
         
-        MPI_Recv(NULL, 0, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &statusRcv);
-        origen = statusRcv.MPI_SOURCE;
-        tag = statusRcv.MPI_TAG;
+        MPI_Recv(&sequence_number, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
+        origen = status.MPI_SOURCE;
+        tag = status.MPI_TAG;
         
-        if (tag >= TAG_PIDO_RECURSO) {
-        	int sequence_number = tag - TAG_PIDO_RECURSO;
+        if (tag == TAG_PIDO_RECURSO) {
 			if(sequence_number > highest_sequence_number_seen) highest_sequence_number_seen = sequence_number;
        	    debug("Me pidieron el recurso");
-        	if(!hay_pedido_local){
+        	if(tengo_el_lock == TRUE){
+                debug("Tengo el lock, tiene que esperar, guardo el pedido");
+                assert(delayedResponses[origen/2] == FALSE);
+                delayedResponses[origen/2] = TRUE;
+            }
+            else if(!hay_pedido_local){
         		debug("Lo otorgo por que no lo necesito");
-       			if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-					MPI_Wait(& requests[id], & status[id]);
-				} else {
-					sentMessage[id] = TRUE;
-				}				
-        		MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, & requests[id] );
+        		MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, &request);
         	}
         	else {
-        		if (sequence_number < our_sequence_number || (sequence_number == our_sequence_number && origen < mi_rank) ){
+        		if (sequence_number < our_sequence_number || (sequence_number == our_sequence_number && (origen/2) < mi_rank) ){
 	        		debug("Lo otorgo por que tiene mayor prioridad");
-           			if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-						MPI_Wait(& requests[id], & status[id]);
-					} else {
-						sentMessage[id] = TRUE;
-					}			
-        			MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, & requests[id] );
+        			MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, &request);
         		}
         		else {
 	        		debug("Tiene menor prioridad, no lo otorgo y guardo el pedido");
@@ -68,8 +61,12 @@ void servidor(int mi_cliente)
         
         if (tag == TAG_OTORGO_RECURSO) {
         	assert(hay_pedido_local == TRUE);
-        	responses ++;
-        	if (responses == n_ranks - 1){// n-1 y saco uno mas por mi rango
+            assert(tengo_el_lock == FALSE);
+            debug("Alguien me dijo que puedo tener el lock");
+            responses ++;
+            //printf("\n SERVER %d, me faltan %d respuestas para tener el lock \n", mi_rank, n_ranks - responses);
+        	if (responses == (n_ranks - 1)){// n-1
+                tengo_el_lock = TRUE;
         	    debug("Dándole permiso");
         		MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
         	}
@@ -79,10 +76,12 @@ void servidor(int mi_cliente)
             assert(origen == mi_cliente);
             debug("Mi cliente solicita acceso exclusivo");
             assert(hay_pedido_local == FALSE);
+            assert(tengo_el_lock == FALSE);
             hay_pedido_local = TRUE;
             our_sequence_number = highest_sequence_number_seen + 1;
             responses = 0;
             if (n_ranks == 1){
+                tengo_el_lock = TRUE;
             	debug("Dándole permiso");
         		MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
             }
@@ -90,12 +89,7 @@ void servidor(int mi_cliente)
                 for (id = 0; id < n_ranks; id++){
 	        		debug("Pido acceso a todos");
             		if (id != mi_rank){
-						if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-							MPI_Wait(& requests[id], & status[id]);
-						} else {
-							sentMessage[id] = TRUE;
-						}			
-            			MPI_Isend(NULL, 0, MPI_INT, id*2, TAG_PIDO_RECURSO + our_sequence_number, COMM_WORLD, & requests[id] );
+            			MPI_Isend(&our_sequence_number, 1, MPI_INT, id*2, TAG_PIDO_RECURSO, COMM_WORLD, &request);
             		}
             	}
             }
@@ -105,16 +99,13 @@ void servidor(int mi_cliente)
             assert(origen == mi_cliente);
             debug("Mi cliente libera su acceso exclusivo");
             assert(hay_pedido_local == TRUE);
+            assert(tengo_el_lock == TRUE);
             hay_pedido_local = FALSE;
+            tengo_el_lock = FALSE;
             for (id = 0; id < n_ranks; id++){
 				if ( delayedResponses[id] == TRUE ){
 	        		debug("Respondo pedidos guardados");
-	        		if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-						MPI_Wait(& requests[id], & status[id]);
-					} else {
-						sentMessage[id] = TRUE;
-					}			
-	            	MPI_Isend(NULL, 0, MPI_INT, id*2, TAG_OTORGO_RECURSO, COMM_WORLD, & requests[id] );
+	            	MPI_Isend(NULL, 0, MPI_INT, id*2, TAG_OTORGO_RECURSO, COMM_WORLD, &request);
 	            	delayedResponses[id] = FALSE;
 	            }
             }
@@ -123,16 +114,14 @@ void servidor(int mi_cliente)
         else if (tag == TAG_TERMINE) {
             assert(origen == mi_cliente);
             debug("Mi cliente avisa que terminó");
+            assert(hay_pedido_local == FALSE);
+            assert(tengo_el_lock == FALSE);
+            assert(listo_para_salir == FALSE);
             listo_para_salir = TRUE;
             withClients--;
             for (id = 0; id < n_ranks; id++){
             	if (id != mi_rank) {
-					if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-						MPI_Wait(& requests[id], & status[id]);
-					} else {
-						sentMessage[id] = TRUE;
-					}			
-            		MPI_Isend(NULL, 0, MPI_INT, id*2, TAG_SIN_CLIENTE, COMM_WORLD, & requests[id] );
+            		MPI_Isend(NULL, 0, MPI_INT, id*2, TAG_SIN_CLIENTE, COMM_WORLD, &request);
             	}
             }
         }
@@ -143,18 +132,13 @@ void servidor(int mi_cliente)
     }
     
     while(withClients > 0) {
-    	MPI_Recv(NULL, 0, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &statusRcv);
-        origen = statusRcv.MPI_SOURCE;
-        tag = statusRcv.MPI_TAG;
+    	MPI_Recv(&sequence_number, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
+        origen = status.MPI_SOURCE;
+        tag = status.MPI_TAG;
         
 		if (tag >= TAG_PIDO_RECURSO) {
        		debug("Otorgo por que ya no necesito el recurso, no tengo cliente");
-   			if(sentMessage[id] == TRUE) { //si ya mande un mensaje espero a que termine, sino marco que voy a mandar el primer mensaje asincronico a ese servidor
-				MPI_Wait(& requests[id], & status[id]);
-			} else {
-				sentMessage[id] = TRUE;
-			}			
-       		MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, & requests[id] );
+       		MPI_Isend(NULL, 0, MPI_INT, origen, TAG_OTORGO_RECURSO, COMM_WORLD, &request);
         }        
         else if (tag == TAG_SIN_CLIENTE) {
         	withClients--;
