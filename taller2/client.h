ac895e4ad214992f5d1b640a09a4ef05484d9a79
ignacio Gleria <igleria@ws22.labo4>
2013-07-23 18:17:21 -0300

    taller 2

---
+++ taller2/client.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+
+
+
+
+
+#define SOCK_PATH "mi_socket"
+#define MENSAJE_MAXIMO 1024
---
+++ taller2/client_2.c
@@ -0,0 +1,51 @@
+#include "client_2.h"
+
+
+
+int main(int argc, char *argv[] )
+{
+    int s, len;
+    struct sockaddr_in remote;
+    char* ip = argv[1];
+	int inet_atom_result = inet_aton(ip, &remote.sin_addr);
+	if( inet_atom_result == 0){
+		perror("horrible tu ip"); 
+		exit(1);
+	} 
+	
+    char str[MENSAJE_MAXIMO];
+
+	/* Crear un socket de tipo UNIX con TCP (SOCK_STREAM). */
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("creando socket ineeeeeeeeeeeeeeeet");
+        exit(1);	
+    }
+
+	/* Establecer la dirección a la cual conectarse. */
+    remote.sin_family = AF_INET;
+    remote.sin_port = htons(5555);
+	len = sizeof(remote);
+
+
+   	/* Conectarse. */
+    if (connect(s, (struct sockaddr *) &remote, len) == -1) {
+        perror("conectandose");
+        exit(1);
+    }
+
+	/* Establecer la dirección a la cual conectarse para escuchar. */
+    while(printf("> "), fgets(str, MENSAJE_MAXIMO, stdin), !feof(stdin)) {
+        if (sendto(s, str, strlen(str) + 1, 0, &remote, len) == -1) {
+            perror("enviando");
+            exit(1);
+        }
+    }
+
+	/* Cerrar el socket. */
+    close(s);
+
+    return 0;
+}
+
+
+
